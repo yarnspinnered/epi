49afe6f9c9d90b4eb5417f6e2c027f4bbd3e3286
Yarnspinner <arbitraryemail25@gmx.com>
2018-08-18 20:18:27 +0800

    solved

--- epi_judge_python/first_missing_positive_entry.py
+++ epi_judge_python/first_missing_positive_entry.py
@@ -2,8 +2,27 @@ from test_framework import generic_test
 
 
 def find_first_missing_positive(A):
-    # TODO - you fill in here.
-    return 0
+    i = 0
+    if not A:
+        return 1
+    while i < len(A):
+        x = A[i]
+        if x < 0 or x >= len(A):
+            i += 1
+        elif x - 1== i:
+            i += 1
+        else:
+            if A[x - 1] != x:
+                A[x - 1], A[i] = A[i], A[x - 1]
+            else:
+                i += 1
+
+
+    for i,x in enumerate(A):
+        if i != x - 1:
+            return i + 1
+
+    return len(A) + 1
 
 
 if __name__ == '__main__':
--- epi_judge_python/gcd.py
+++ epi_judge_python/gcd.py
@@ -2,8 +2,32 @@ from test_framework import generic_test
 
 
 def gcd(x, y):
-    # TODO - you fill in here.
-    return 0
+    def is_even(x):
+        return x & 1 == 0
+
+    if x == 0:
+        return y
+    elif y == 0:
+        return x
+    elif x == y:
+        return x
+
+    if x > y:
+        big = x
+        small = y
+    else:
+        big = y
+        small = x
+
+    if is_even(big) and is_even(small):
+        return gcd(big >> 1, small >> 1) << 1
+    elif not is_even(big) and not is_even(small):
+        return gcd(big - small, small)
+    elif is_even(big) and not is_even(small):
+        return gcd(big >> 1, small)
+    else:
+        return gcd(big, small >> 1)
+
 
 
 if __name__ == '__main__':
