fc1d714ac1ea9d4ef9b660fd06bafd745fed4a90
Yarnspinner <arbitraryemail25@gmx.com>
2018-09-10 21:24:46 +0800

    solved

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,11 +1,63 @@
 from test_framework import generic_test
-
-
+from collections import deque
+import string
 # Uses BFS to find the least steps of transformation.
-def transform_string(D, s, t):
-    # TODO - you fill in here.
-    return 0
+# dictionary method
+# def transform_string(D, s, t):
+#     def check_adjacent(a,b):
+#         if len(a) != len(b) or a == b:
+#             return False
+#         diff = 0
+#         i = 0
+#         while i < len(a) and diff <= 1:
+#             if a[i] != b[i]:
+#                 diff += 1
+#             i += 1
+#         return diff <= 1
+#
+#     q = deque([(0,s)])
+#     explored = {s}
+#
+#     while q:
+#         steps, curr = q.pop()
+#         if curr == t:
+#             return steps
+#         for w in D:
+#             if check_adjacent(w, curr) and w not in explored:
+#                 explored.add(w)
+#                 q.appendleft((steps + 1, w))
+#
+#
+#     return -1
 
+def transform_string(D, s, t):
+    def check_adjacent(a,b):
+        if len(a) != len(b) or a == b:
+            return False
+        diff = 0
+        i = 0
+        while i < len(a) and diff <= 1:
+            if a[i] != b[i]:
+                diff += 1
+            i += 1
+        return diff <= 1
+
+    q = deque([(0,s)])
+    explored = {s}
+
+    while q:
+        steps, curr = q.pop()
+        if curr == t:
+            return steps
+        for i in range(len(curr)):
+            for c in string.ascii_lowercase:
+                w = curr[0:i] + c + curr[i + 1:]
+                if w in D and w not in explored:
+                    explored.add(w)
+                    q.appendleft((steps + 1, w))
+
+
+    return -1
 
 if __name__ == '__main__':
     exit(
