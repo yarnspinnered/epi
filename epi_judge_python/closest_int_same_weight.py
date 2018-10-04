12ef4be98446ae2672ecba74faedb2cf5f638d30
Yarnspinner <arbitraryemail25@gmx.com>
2018-10-04 22:17:07 +0800

    solved

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -2,7 +2,18 @@ from test_framework import generic_test
 
 
 def closest_int_same_bit_count(x):
-    # TODO - you fill in here.
+    smallest_0 = None
+    smallest_1 = None
+
+    for i in range(64):
+        current = x >> i & 1
+        if current == 0:
+            smallest_0 = i
+        else:
+            smallest_1 = i
+        if not smallest_0 is None and not smallest_1 is None:
+            BIT_MASK = (1 << smallest_0) | (1 << smallest_1)
+            return x ^ BIT_MASK
     return 0
 
 
